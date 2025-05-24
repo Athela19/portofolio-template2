e8e602d434eef16de5015c0f03088bdac1e4d11f
Syarif <syarkev@gmail.com>
2025-05-24 13:19:59 +0700

    fix bug

--- app/project/fastknuck/auth/page.jsx
+++ app/project/fastknuck/auth/page.jsx
@@ -1,6 +1,6 @@
 // app/project/fastknuck/auth/page.jsx
 import { Suspense } from "react";
-import AuthPage from "@/project/fastknuck/auth/auth";
+import AuthPage from "../../../../project/fastknuck/auth/auth";
 
 export default function Page() {
   return (
