59affba7f0d0a73d8846a36e82c47ec303ee1e70
Syarif <syarkev@gmail.com>
2025-05-24 17:21:26 +0700

    fix bug

--- app/project/fastknuck/page.jsx
+++ app/project/fastknuck/page.jsx
@@ -1,8 +1,6 @@
 "use client"
-
-import FastKnuck from "@/project/fastknuck/home";
 export default function FastKnuckPage() {
     return (
-       <FastKnuck />
+       <h1>FastKnuck</h1>
     );
 }
